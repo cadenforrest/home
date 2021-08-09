78500b1bdf39573914664d5d2b0f27b02fcc1bce
cadenforrest <cadenforrest@gmail.com>
2021-08-09 02:44:18 -0500

    fixed middle name

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -7,6 +7,7 @@ const navBar = {
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Caden",
+  middleName: "",
   lastName: "Westmoreland",
   message: "Engineer | Communicator | Thinker | Problem Solver | Friend ",
   icons: [
