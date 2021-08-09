ac804aa4e9e3f8569403f292b8bbc3768e7c82de
cadenforrest <cadenforrest@gmail.com>
2021-08-09 02:42:58 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.f010477370d0f66b61f832114c7fea13.js"
+  "/home/precache-manifest.cd1a083839f47ae769503e8f65689a67.js"
 );
 
 self.addEventListener('message', (event) => {
